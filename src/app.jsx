f39bb4bffa230d5d9028244b4d342ad4d800fd42
m-m-black <16482752+m-m-black@users.noreply.github.com>
2024-06-03 15:41:13 +1000

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./pages/home/index";
+import "./styles/styles.css";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import homeStyles from "./home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className={homeStyles.container}>This is a template</div>
+    </>
+  );
+}
